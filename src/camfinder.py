928378ce64f6f4bcf68eb18568502f38a6ec25cb
glebite <glebite@gmail.com>
2022-08-21 14:15:15 -0400

    Adding in source as is for now - will continue to work on this.

---
+++ src/camfinder.py
@@ -0,0 +1,150 @@
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import requests
+from requests.models import Response
+from urllib3.exceptions import ReadTimeoutError
+import re
+import sys
+import threading
+import logging
+
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+                    filename='camfinder.log',level=logging.DEBUG)
+
+
+NEXT = '»'
+
+'''
+These are stream sources that "stream" instead of providing
+a single image.  Until there is fix for that to retrieve just
+a static image, the code will "continue"
+'''
+PROBLEMATIC_STREAM_SOURCES = [
+    'mjpg',
+    'mjpeg',
+    'videostream',
+    'GetOneShot',
+    'action=stream',
+    'GetData',
+    'cam_1.cgi',
+    'faststream',
+    'asp/video.cgi'
+    ]
+
+
+headers = {'User-Agent':
+           'Mozilla/5.0 (Windows NT 6.1; WOW64)'
+           ' AppleWebKit/537.36 (KHTML, like Gecko)'
+           ' Chrome/56.0.2924.76 Safari/537.36'}
+
+
+global results
+results = list()
+
+
+def update_camera_url(replace, image, counter):
+    source_url = image['src']
+    logging.info(f'update_camera_url: {source_url=}')
+    if replace:
+        source_url = re.sub(f'{replace}(\d+)',
+                            f'{replace}{counter}', source_url)
+    else:
+        source_url = image['src']
+    logging.info(f'update: {source_url=}')
+    return source_url
+
+
+def find_multi_cam(image):
+    base = 0
+    replace = None
+    logging.info(f"image data: {image['src']=}")
+    if 'channel=' in image['src']:
+        base = 1
+        replace = "channel="
+    if 'chn=' in image['src']:
+        replace = "chn="
+        
+    return base, replace
+
+def get_image(image_tuple):
+    global results
+
+    # logging.info(f'{image_tuple=}')
+    image, page, counter = image_tuple
+    base, replace = find_multi_cam(image)
+    
+    logging.info(f'{base=} {replace=}')
+    for i in range(base, 32):
+        s = update_camera_url(replace, image, i)
+        # logging.info(f'{replace=}')
+        # address = re.findall(r'(d{1,3}\.d{1,3}\.d{1,3}\.d{1,3})', replace)
+        # print(address.groups)
+        logging.info(f'Updated information {page=} {i=} {address=}')
+        last_img = Response()
+        last_img._content = None
+        try:
+            img = requests.get(s, headers=headers, timeout=30)
+            if img.content != last_img.content:
+                # logging.info(f'Getting: {replace=} {i=} {s=}')                
+                last_img._content = img.content
+                file_name = f'{page}-{base}-{counter}-remote.jpg'
+                with open(file_name, 'wb') as f:
+                    f.write(img.content)
+                results.append((file_name, s, image))
+            else:
+                break
+        except Exception as e:
+            logging.error(f'Exception: {e} {i=} {s=}')
+            break
+        if not replace:
+            break
+
+
+def output_html(results):
+    with open('index.html', 'w') as fp:
+        fp.write('<html><body>')
+        logging.debug(f'{type(results)}')
+        for (image_filename, s, image) in results:
+            fp.write(f'<br/><img src="./{image_filename}"'
+                     ' width="800" height="600" ></img>')
+            fp.write(f'<br/>{s}<br/>{image["src"]}<br/> {image["title"]}')
+        fp.write('</body></html')
+
+
+def main(location):
+    global results
+    page = 1
+    executor = ThreadPoolExecutor(50)
+    while True:
+        if page > 1:
+            URL = f'http://www.insecam.org/en/bycountry/{location}/?page={page}'
+        else:
+            URL = f'http://www.insecam.org/en/bycountry/{location}'
+        page_data= requests.get(URL, headers=headers)
+
+        soup = BeautifulSoup(page_data.content, 'html.parser')
+
+        images = soup.find_all("img",
+                               {"class":
+                                "thumbnail-item__img img-responsive"})
+
+        for counter, image in enumerate(images):
+            if re.compile('|'.join(PROBLEMATIC_STREAM_SOURCES),
+                          re.IGNORECASE).search(image['src']):
+                continue
+
+            t = executor.submit(get_image, (image, page, counter))
+                                
+        nextp = re.search(f'page={page+1}', str(page_data.content))
+        if nextp:
+            page += 1
+            print('.', end='')
+        else:
+            break
+    print("")
+    results = list(dict.fromkeys(results))
+    output_html(results)
+
+
+main(sys.argv[1])
