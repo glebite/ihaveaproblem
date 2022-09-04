52e979b0dbbcfb6dc092749447611241e7d4b8f7
glebite <glebite@gmail.com>
2022-09-03 23:27:15 -0400

    Stupid comment only today.

--- src/camfinder.py
+++ src/camfinder.py
@@ -35,6 +35,7 @@ PROBLEMATIC_STREAM_SOURCES = [
     ]
 
 
+# Supply something for the server to not choke on
 headers = {'User-Agent':
            'Mozilla/5.0 (Windows NT 6.1; WOW64)'
            ' AppleWebKit/537.36 (KHTML, like Gecko)'
