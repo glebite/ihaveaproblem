5a6a7e14069b50e4f3fa6d4b5d63f8fd9820f39a
glebite <glebite@gmail.com>
2022-11-16 20:54:26 -0500

    Can capture an image now.

--- src/streamwork.py
+++ src/streamwork.py
@@ -7,8 +7,8 @@ import requests
 import numpy as np
 
 
-# URL = 'http://185.72.27.244:85/mjpg/video.mjpg'
-URL = 'http://37.221.17.136:9000/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER'
+URL = 'http://185.72.27.244:85/mjpg/video.mjpg'
+# URL = 'http://37.221.17.136:9000/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER'
 
 
 r = requests.get(URL, stream=True)
@@ -22,8 +22,7 @@ if(r.status_code == 200):
             jpg = bytes[a:b+2]
             bytes = bytes[b+2:]
             i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-            cv2.imshow('i', i)
-            if cv2.waitKey(1) == 27:
-                exit(0)
+            cv2.imwrite("capture.jpg", i)
+            break
 else:
     print("Received unexpected status code {}".format(r.status_code))
